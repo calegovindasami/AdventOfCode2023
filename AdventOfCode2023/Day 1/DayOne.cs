6b212bda5e42dd27d0cd95f3cb0634e12255b9dd
ST10083941 <96740648+cgov-0406@users.noreply.github.com>
2023-12-05 11:06:11 +0200

    Add project files.

---
+++ AdventOfCode2023/Day 1/DayOne.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Day_1
+{
+    public static class DayOne
+    {
+        static Dictionary<string, string> Digits = new()
+        {
+            { "zero", "z0o" },
+            { "one", "o1e" },
+            { "two", "t2o" },
+            { "three", "t3e" },
+            { "four", "f4r" },
+            { "five", "f5e" },
+            { "six", "s6x" },
+            { "seven", "s7n" },
+            { "eight", "e8t" },
+            { "nine", "n9e" }
+        };
+        public static int GetCalibrationValue(string line)
+        {
+            foreach (var pair in Digits)
+            {
+                string pattern = $@"{pair.Key}";
+                line = Regex.Replace(line, pattern, pair.Value);
+            }
+
+            char[] numbers = line.Where(x => char.IsNumber(x)).ToArray();
+
+            string combinedNumber = numbers[0].ToString() + numbers[numbers.Length - 1].ToString();
+            return int.Parse(combinedNumber);
+        }
+
+        public static int Solution()
+        {
+            int sum = 0;
+            string[] lines = File.ReadAllLines("C:\\Users\\caleg\\source\\repos\\AdventOfCode2023\\AdventOfCode2023\\Day 1\\input.txt");
+
+            foreach (string line in lines)
+            {
+                int calibVal = GetCalibrationValue(line);
+                sum += calibVal;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,13 @@
+﻿using AdventOfCode2023.Day_1;
+
+namespace AdventOfCode2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = DayOne.Solution();
+            Console.WriteLine(total);
+        }
+    }
+}
